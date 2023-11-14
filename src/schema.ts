df418a1ef56cb43a404b14250baa306ad81382d0
Prottoy Raha <135445676+ItsPinion@users.noreply.github.com>
2023-11-14 20:15:47 +0600

    added zod for data validation

---
+++ src/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const createNoteSchema = z.object({
+    text: z.string().min(5).max(500),
+    date: z.date().min(new Date()).optional()
+})
+export const updateNoteSchema = z.object({
+    text: z.string().min(5).max(500).optional(),
+    date: z.date().min(new Date()).optional()
+})
